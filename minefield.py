00eb647cca59f475c44e06564eff8a151e2bbab7
PluharT <pluhar.tomi@gmail.com>
2020-05-05 18:35:05 +0200

    implemented game_generate_neighbours

--- minefield.py
+++ minefield.py
@@ -7,6 +7,16 @@ class MineField:
     Field_rows = 0
     Field_columns = 0
     Field_mines = 0
+    Game_neighbour_check_tuple = (
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, -1],
+        [0, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1]
+    )
 
     def set_attributes(self, rows=0, columns=0, mines=0):
         """Sets one MineFields attributes, raises ValueError if the parameters are negative"""
@@ -25,9 +35,9 @@ class MineField:
         """Generates the field by creating a two dimensional list, returns if self.rows or self.columns are 0"""
         if (self.Field_columns or self.Field_rows) == 0:
             return
-        for i in range(0, self.Field_rows):
+        for i in range(self.Field_rows):
             temp_list = []
-            for j in range(0, self.Field_columns):
+            for j in range(self.Field_columns):
                 temp_list.append([0, 0])
             self.Cell_array.append(temp_list)
 
@@ -35,7 +45,7 @@ class MineField:
         """
         generates mines in the Cell_array, by setting the first variable to -1
         returns if there are more mines than cells
-        can set specific cells as mines, with args
+        can set specific cells as mines, with cells
         :type cells: list
         """
         rows = self.Field_rows
@@ -59,10 +69,29 @@ class MineField:
                     self.Cell_array[current_cell[0]][current_cell[1]][0] = -1
                     i += 1
 
+    def game_generate_neighbours(self):
+        """
+        Goes through each cell, and counts how many of it's neighbouring cells are mines, if itself is not a mine
+        Stores the counted value
+        """
+        rows = self.Field_rows
+        columns = self.Field_columns
+        check = self.Game_neighbour_check_tuple
+        for i in range(rows):
+            for j in range(columns):
+                if self.Cell_array[i][j][0] == -1:
+                    continue
+                temp_n_mines = 0
+                for k in check:
+                    if 0 <= i + k[0] < rows and 0 <= j + k[1] < columns:
+                        temp_n_mines += 1 if self.Cell_array[i + k[0]][j + k[1]][0] == -1 else 0
+                self.Cell_array[i][j][0] = temp_n_mines
+
 
 # ----------T-E-S-T----------
 lol = MineField()
-lol.set_attributes(3, 3, 3)
+lol.set_attributes(1, 1)
 lol.game_generate_cell_array()
 lol.game_generate_mines()
+lol.game_generate_neighbours()
 print(lol.Cell_array)
